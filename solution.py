d380eb1d9e635af529d99d8da8d39b1398c9191d
Hcheema4 <90523906+Hcheema4@users.noreply.github.com>
2021-12-04 16:38:21 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -142,7 +142,7 @@ def get_route(hostname):
                     #Fill in end
                 elif types == 0:
                     bytes = struct.calcsize("d")
-                    timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
+                    #timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
                     #Fill in start
                     #You should add your responses to your lists here and return your list if your destination IP is met
                     print(" %d rtt=%.0f ms %s" %(ttl,(timeReceived -t) * 1000, addr[0]))
